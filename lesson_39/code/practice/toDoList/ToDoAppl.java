d96cdf5b01d265a674f976fa1edf5629fefd0e1d
narol01 <kmingogod@gmail.com>
2023-11-07 23:58:53 +0100

    toDoList

--- lesson_39/code/practice/toDoList/ToDoAppl.java
+++ lesson_39/code/practice/toDoList/ToDoAppl.java
@@ -22,57 +22,61 @@ public class ToDoAppl {
         }
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to ToDoList Menu");
-        while (true) {
-            MenuTask.printMenu();
-            System.out.print("Выберите действие: ");
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1: {
-                    while (true) {
-                        System.out.print("Ввод дaнных");
-                        System.out.print(" \nВведите task:");
-                        String task = scanner.next();
-                        Task task1 = new Task(toDoList.qtv()+1, task,toDoList.qtv()+1);
-                        toDoList.addRecords(task1);
+        try {
+            while (true) {
+                MenuTask.printMenu();
+                System.out.print("Выберите действие: ");
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1: {
+                        while (true) {
+                            System.out.print("Ввод дaнных");
+                            System.out.print(" \nВведите task:");
+                            String task = scanner.next();
+                            Task task1 = new Task(toDoList.qtv() + 1, task, toDoList.qtv() + 1);
+                            toDoList.addRecords(task1);
+                            toDoList.viewRecords();
+                            System.out.println("Дополнить ещё \n да-продожить \n нет-закончить");
+                            String a = scanner.next();
+                            if (a.equals("нет")) {
+                                break;
+                            }
+                        }
+                    }
+                    case 2: {
+                        System.out.println("Your tasks: ");
                         toDoList.viewRecords();
-                        System.out.println("Дополнить ещё \n да-продожить \n нет-закончить");
-                        String a = scanner.next();
-                        if (a.equals("нет")) {
-                            break;
-                        }
+                        break;
                     }
-                }
-                case 2: {
-                    System.out.println("Your tasks: ");
-                    toDoList.viewRecords();
-                    break;
-                }
-                case 3: {
-                    System.out.print("Введите номер записи для удаления: ");
-                    int recordNumber = scanner.nextInt();
-                    if (taski[recordNumber] == null) {
-                        System.out.println("Список записей пуст. Нечего удалять.");
-                    } else {
-                        if (recordNumber < 1 || recordNumber > taski.length) {
-                            System.out.println("Неверный номер записи.");
+                    case 3: {
+                        System.out.print("Введите номер записи для удаления: ");
+                        int recordNumber = scanner.nextInt();
+                        if (taski[recordNumber] == null) {
+                            System.out.println("Список записей пуст. Нечего удалять.");
                         } else {
-                            toDoList.deleteRecords(recordNumber);
-                            System.out.println("Запись удалена.");
+                            if (recordNumber < 1 || recordNumber > taski.length) {
+                                System.out.println("Неверный номер записи.");
+                            } else {
+                                toDoList.deleteRecords(recordNumber);
+                                System.out.println("Запись удалена.");
+                            }
                         }
+                        break;
                     }
-                    break;
-                }
-                case 4: {
-                    System.out.println("Выход из приложения.");
-                    System.exit(0);
-                    break;
-                }
+                    case 4: {
+                        System.out.println("Выход из приложения.");
+                        System.exit(0);
+                        break;
+                    }
 
-                //раскопировать cases;
-                default: {
-                    System.out.println("Неверный выбор");
+                    //раскопировать cases;
+                    default: {
+                        System.out.println("Неверный выбор");
+                    }
                 }
             }
+        }catch (Exception e){
+            System.out.println(e.getMessage()+","+"try again");
         }
     }
 }
