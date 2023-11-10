f5c3945bdcb83fc44838620c62c8f163c43b2817
narol01 <kmingogod@gmail.com>
2023-11-10 11:10:27 +0100

    ilist

--- lesson_44/code/practice/ilist/Ilist.java
+++ lesson_44/code/practice/ilist/Ilist.java
@@ -1,25 +1,41 @@
 package practice.ilist;
 
-public interface Ilist<E> {
+public interface Ilist<E> extends Iterable<E>{
     int size();// узнать размер списка
-    boolean isEmpty();// есть ли что-то в списке или он пустой
+
+    default boolean isEmpty()// есть ли что-то в списке или он пустой
+    {
+        return size() == 0;
+    }
+
     void clean();//Удалить всё из мписка
+
     boolean add(E element);// добавить элемент списка
-    boolean contains(Object o);//есть ли элемент в списке
-    boolean remove(Object o);// удалить элемент, если он есть (а все ли элементы мы удалим?)
-    // добавить (вставить) элемент по индексу
 
-    // удалить элемент из списка по индексу
+    default boolean contains(Object o)//есть ли элемент в списке
+    {
+        return indexOf(o)>=0;
+    }
 
-    // обновить элемент в списке по его индексу
+    default boolean remove(Object o)// удалить элемент по его индексу, если он есть (а все ли элементы мы удалим?)
+    {
+         int index=indexOf(o);
+         if(index < 0) {//когда обьекта нет в нашем списке
+             return false;
+         }
+         remove(index);
+         return true;
+    }
 
-    // есть ли элемент в списке
+    boolean add(int index, E element);// добавить (вставить) элемент по индексу
 
-    // найти элемент по его индексу
+    E remove(int index);// удалить элемент из списка по индексу
 
-    // зная элемент, найти его индекс
+    E get(int index);//получить  элемент по его индексу
 
-    // удалить все из списка
+    E set(int index, E element);// обновить элемент в списке по его индексу
 
-    // ищем индекс элемента с конца списка
+    int indexOf(Object o);// зная элемент, найти его индекс
+
+    int lastindexOf(Object o);// ищем индекс элемента с конца списка
 }
---
+++ lesson_44/code/practice/ilist/IlistImpl.java
@@ -0,0 +1,56 @@
+package practice.ilist;
+
+import java.util.Iterator;
+
+public class IlistImpl implements Ilist{
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clean() {
+
+    }
+
+    @Override
+    public boolean add(Object element) {
+        return false;
+    }
+
+    @Override
+    public boolean add(int index, Object element) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastindexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
