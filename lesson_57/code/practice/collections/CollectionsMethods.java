2bd919a22828d5d79fcd42157b26088ed209a734
narol01 <kmingogod@gmail.com>
2023-12-01 10:06:13 +0100

    Supermarket

---
+++ lesson_57/code/practice/collections/CollectionsMethods.java
@@ -0,0 +1,16 @@
+package practice.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionsMethods {
+    // Создайте список известных вам фруктов.
+    // Выполните поиск имеющегося и отсутствующего элемента списка.
+    // Отсортируйте этот список по алфавиту в прямом и обратном порядке.
+    // Проверьте работу методов
+    // max(Collection<?> coll)
+    // min(Collection<?> coll), что они возвращают?
+
+    List<String> fruits=new ArrayList<>();
+    fruits.add("apple","orange","banana","apple","pear","plum","strawbery");
+}
