613b7f8170130cf826cfc4de9ee90848f2b182ab
narol01 <kmingogod@gmail.com>
2023-12-13 11:07:45 +0100

    up

--- lesson_66/code/homework/train/TrainDataAppl.java
+++ lesson_66/code/homework/train/TrainDataAppl.java
@@ -19,7 +19,12 @@ public class TrainDataAppl {
         printCells(cells);
         double sum=0;
         int counter=0;
+        int counterMale=0;
+        int counterDeatherMale=0;
+        int counterFemale=0;
+        int counterDeathFemale=0;
         int counterYounger18=0;
+        int counterDeathYounger18=0;
         double priceFirst=0;
         int countFirst=0;
         double priceSecond=0;
@@ -31,6 +36,20 @@ public class TrainDataAppl {
             cells=str.split(",");
                         //СКолько было пассажиров младше 18
             if(!(cells[6].isEmpty())){if (Double.parseDouble(cells[6])<=18){counterYounger18++;}}
+                        //количество выживших мужчин и женщин
+            if (cells[5].equalsIgnoreCase("male") && Integer.parseInt(cells[1])==0){
+                counterMale++;}
+            if (cells[5].equalsIgnoreCase("female") && Integer.parseInt(cells[1])==0){
+                counterFemale++;
+            }
+                        //количество умерших мужчин и женщин
+            if (cells[5].equalsIgnoreCase("male") && Integer.parseInt(cells[1])==1){
+                counterDeatherMale++;}
+            if (cells[5].equalsIgnoreCase("female") && Integer.parseInt(cells[1])==1){
+                counterDeathFemale++;
+            }
+                        //количество умерших детей
+            if(!(cells[6].isEmpty())){if (Double.parseDouble(cells[6])<=18 && Integer.parseInt(cells[1])==1){counterDeathYounger18++;}}
                         //средняя цена по классах
             switch (Integer.parseInt(cells[2])){
                 case 1 ->{
@@ -57,7 +76,15 @@ public class TrainDataAppl {
         }
         System.out.println("Sum total price: "+sum);
         System.out.println("All passenger: "+counter);
+        System.out.println("All Survived: "+ (counterMale+counterFemale));
+        System.out.println("All Death: "+ (counter-(counterMale+counterFemale)));
+        System.out.println("Death Male: "+ counterMale);
+        System.out.println("Survived Male: "+ counterDeatherMale);
+        System.out.println("Death Female: "+ counterDeathFemale);
+        System.out.println("Survived Female: "+ counterFemale);
         System.out.println("All passenger younger 18: "+counterYounger18);
+        System.out.println("All Death passenger younger 18: "+counterDeathYounger18);
+        System.out.println("All Survived passenger younger 18: "+(counterYounger18-counterDeathYounger18));
         System.out.println("Avg first: "+priceFirst/countFirst);
         System.out.println("Avg second: "+priceSecond/count2);
         System.out.println("Avg three: "+price3/count3);
